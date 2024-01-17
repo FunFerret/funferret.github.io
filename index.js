307fa4448ef4b5c111e60726720c0a85de250c78
FunFerret <cfonferek@gmail.com>
2024-01-17 13:28:10 -0600

    update

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const Creatures = [ 'Acrocanthosaurus','Apatosaurus','Arganodus','Auroraceratops','Coahuilaceratops','Elasmosaurus','HorseshoeCrab','Kronosaurus','Ichthyovenator','Lurdusaurus','Megalosaurus','Megaraptor','Mosasaurus','Oryctodromeus','Pachycephalosaurus','Parasaurolophus','Pteranodon','Saichania','Tropeognathus','Tyrannosaurus','Utahraptor','Velociraptor'];
-const Herbivores = ['Apatosaurus','Auroraceratops','Coahuilaceratops','HorseshoeCrab','Lurdusaurus','Oryctodromeus','Pachycephalosaurus','Parasaurolophus','Saichania'];
-const Carnivores = ['Acrocanthosaurus','Ichthyovenator','Megalosaurus','Megaraptor','Tyrannosaurus','Utahraptor','Velociraptor'];
-const Flyers = ['Pteranodon','Tropeognathus'];
-const Aquatics = ['Arganodus','Elasmosaurus','Kronosaurus','Mosasaurus'];
+var Creatures = new Array('Acrocanthosaurus','Apatosaurus','Arganodus','Auroraceratops','Coahuilaceratops','Elasmosaurus','HorseshoeCrab','Kronosaurus','Ichthyovenator','Lurdusaurus','Megalosaurus','Megaraptor','Mosasaurus','Oryctodromeus','Pachycephalosaurus','Parasaurolophus','Pteranodon','Saichania','Tropeognathus','Tyrannosaurus','Utahraptor','Velociraptor');
+var Herbivores = new Array('Apatosaurus','Auroraceratops','Coahuilaceratops','HorseshoeCrab','Lurdusaurus','Oryctodromeus','Pachycephalosaurus','Parasaurolophus','Saichania');
+var Carnivores = new Array('Acrocanthosaurus','Ichthyovenator','Megalosaurus','Megaraptor','Tyrannosaurus','Utahraptor','Velociraptor');
+var Flyers = new Array('Pteranodon','Tropeognathus');
+var Aquatics = new Array('Arganodus','Elasmosaurus','Kronosaurus','Mosasaurus');
 
-//used by header buttons to set the div state as well as the button text
-function SHfuntion(loc) {
-    var select = document.getElementById(loc),
-        but = document.getElementById(loc.concat("button"));
-    if (select.style.display === "none") {
-        select.style.display = "block";
-        but.textContent = "Hide".concat(but.textContent.slice(4));
-    } else {
-        select.style.display = "none";
-        but.textContent = "Show".concat(but.textContent.slice(4));
-    }
-}
+
+
